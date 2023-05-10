c71ba1ac9dbf80faf02658b0c18826a78da1485b
Fujipp <ploy19072004@gmail.com>
2023-05-10 15:39:34 +0700

    Initial commit

---
+++ BankingApplication1/src/bankingapplication1/Account.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankingapplication1;
+
+/**
+ *
+ * @author Student
+ */
+public class Account {
+    private int number;
+    private String name;
+    private double balance;
+
+    public Account(int number, String name, double balance) {
+        this.number = number;
+        this.name = name;
+        this.balance = balance;
+    }
+    
+    public void deposit(double amount) {
+        this.balance = this.balance + amount;
+    }
+    
+    public void withdraw(double amount) {
+        this.balance = this.balance - amount;
+    }
+    
+    public double getBalance() {
+        return this.balance;
+    }
+    
+    public double getNumber() {
+        return this.number;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
